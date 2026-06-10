fd06be4a0c8765085873c3bc9ad0027ac9396ef2
Maxim <mudrak.maksym@chnu.edu.ua>
2026-06-10 15:13:20 +0300

    find game fixed

--- src/components/MatchmakingWatcher/index.tsx
+++ src/components/MatchmakingWatcher/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { findGame } from '@/api/gameApi';
 import { socket } from '@/socket/socket'
 import { useUserStore } from '@/store/userStore';
 import { useMatchmakingStore } from '@/store/matchmakingStore';
@@ -12,6 +13,7 @@ export default function MatchmakingWatcher() {
     const token = useUserStore(s => s.token);
 
     const isSearching = useMatchmakingStore(s => s.isSearching);
+    const setEloRange = useMatchmakingStore(s => s.setEloRange);
     const clearMatchmaking = useMatchmakingStore(s => s.clearMatchmaking);
 
     useEffect(() => {
@@ -35,6 +37,32 @@ export default function MatchmakingWatcher() {
             socket.off('match:found', handleMatchFound);
         };
     }, [isSearching, user, token, clearMatchmaking, navigate]);
+
+    useEffect(() => {
+        if (!isSearching || !token) return;
+
+        const updateRange = async () => {
+            try {
+                const data = await findGame(token);
+
+                if (data.status === 'searching') {
+                    setEloRange(data.eloRange);
+                }
+
+                if (data.status === 'matched' || data.status === 'in_game') {
+                    clearMatchmaking();
+                    navigate(`/game/${data.game._id}`);
+                }
+            } catch (error) {
+                console.log(error);
+                clearMatchmaking();
+            }
+        };
+
+        const interval = setInterval(updateRange, 3000);
+
+        return () => clearInterval(interval);
+    }, [isSearching, token, setEloRange, clearMatchmaking, navigate]);
 
     return null;
 }
