d28f3153b92b2d1b36f405629a37af0656cea5a9
Maxim <mudrak.maksym@chnu.edu.ua>
2026-06-21 13:02:46 +0300

    updated keycard copy button

--- src/pages/AdminPage/components/Cards/KeyCard/index.tsx
+++ src/pages/AdminPage/components/Cards/KeyCard/index.tsx
@@ -1,5 +1,6 @@
 import s from './styles.module.scss'
 import Button from '@/components/UI/Button'
+import { useState } from 'react';
 
 type Props = {
     keyValue: string;
@@ -7,15 +8,27 @@ type Props = {
 }
 
 export default function KeyCard(props: Props) {
+    const [copied, setCopied] = useState(false);
+
+    async function handleCopy() {
+        await navigator.clipboard.writeText(props.keyValue);
+
+        setCopied(true);
+
+        setTimeout(() => {
+            setCopied(false);
+        }, 2000);
+    }
+    
     return (
         <div className={s.key_card}>
             <p className={s.title}>Code</p>
             <p className={s.value}>{props.keyValue}</p>
             <Button
-                text='Copy'
+                text={copied ? 'Copied!' : 'Copy'}
                 variant='profile'
                 animation='white-hover'
-                onClick={() => navigator.clipboard.writeText(props.keyValue)}
+                onClick={handleCopy}
                 className={s.button}
             />
             <Button
